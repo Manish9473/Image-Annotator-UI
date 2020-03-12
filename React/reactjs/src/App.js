0242792dcd99ea36e1014418faa607148fb1fe88
Manish Kumar (B17CS032) <kumar.53@iitj.ac.in>
2020-03-12 22:34:27 +0530

    react files

---
+++ React/reactjs/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <img src="roads.JPG" height="0" width="0" />
+         
+      
+        
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
